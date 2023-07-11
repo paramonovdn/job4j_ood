1728c69ab40275070af1df9d22fa4b4002a7579d
paramonovdn@mail.ru <paramonovdn@mail.ru>
2023-07-11 12:26:26 +0300

    4. Шаблонизатор.

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,61 @@
+package ru.job4j.template;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.ood.tdd.*;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class GeneratorTest {
+
+    @Test
+    public void simpleTest() {
+        Generator generator = new Generator();
+        String pattern = "I am a ${name}, Who are ${subject}? ";
+        String expected = "I am a human, Who are you? ";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "human" );
+        map.put("subject", "you" );
+        String result = generator.produce(pattern, map);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void testWithWrongArgument() {
+        Generator generator = new Generator();
+        String pattern = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "human" );
+        assertThatThrownBy(() -> generator.produce(pattern, map)).
+                isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void testWithWrongArgument2() {
+        Generator generator = new Generator();
+        String pattern = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "human" );
+        map.put("object", "you" );
+        assertThatThrownBy(() -> generator.produce(pattern, map)).
+                isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void testWithExtraArgument() {
+        Generator generator = new Generator();
+        String pattern = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "human" );
+        map.put("subject", "you" );
+        map.put("subject", "me" );
+        assertThatThrownBy(() -> generator.produce(pattern, map)).
+                isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+}
