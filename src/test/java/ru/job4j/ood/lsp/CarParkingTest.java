1184529d72adeac33bddc10147ab070eede41484
paramonovdn@mail.ru <paramonovdn@mail.ru>
2023-07-20 23:37:57 +0300

    2. Парковка машин [#853 # [#853 #431103]]

--- src/test/java/ru/job4j/ood/lsp/CarParkingTest.java
+++ src/test/java/ru/job4j/ood/lsp/CarParkingTest.java
@@ -2,7 +2,12 @@ package ru.job4j.ood.lsp;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import ru.job4j.ood.lsp.parking.Car;
+import ru.job4j.ood.lsp.parking.CarParking;
+import ru.job4j.ood.lsp.parking.ControlParking;
+import ru.job4j.ood.lsp.parking.GeneralCarParking;
 import java.util.ArrayList;
+import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
